fa9fdb5fe95bb92b1658eb6bd1862eeac92a78e6
Sudharshan <solderneer@gmail.com>
2021-08-10 12:43:42 +0800

    feat: added mission log page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,172 @@
 import 'package:flutter/material.dart';
 
+class LogPage extends StatefulWidget {
+  LogPage({Key? key}) : super(key: key);
+
+  static Route route() {
+    return MaterialPageRoute<void>(builder: (_) => HomePage());
+  }
+
+  @override
+  _LogPageState createState() => _LogPageState();
+}
+
+class _LogPageState extends State<LogPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: SafeArea(
+            child: SingleChildScrollView(
+                child: Container(
+      padding: EdgeInsets.all(30.0),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+          Widget>[
+        Container(
+            height: 400,
+            padding: EdgeInsets.all(30.0),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.all(
+                Radius.circular(15.0),
+              ),
+            ),
+            child: Center(
+                child:
+                    Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+              Text("Review your knowledge",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline4?.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.w500)),
+              Container(
+                  margin: EdgeInsets.only(top: 10.0),
+                  child: Text("To boost your retention",
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context)
+                          .textTheme
+                          .subtitle1
+                          ?.copyWith(color: Colors.white)))
+            ]))),
+        Container(
+          margin: EdgeInsets.only(top: 20.0),
+          child: Text("Challenges",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  ?.copyWith(fontSize: 24)),
+        ),
+        SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: EdgeInsets.only(top: 10.0),
+            child: Row(children: <Widget>[
+              Container(
+                  width: 300,
+                  height: 200,
+                  padding: EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text("✨ Draw a boot",
+                            style: Theme.of(context).textTheme.headline5),
+                        Container(
+                            margin: EdgeInsets.only(top: 5.0),
+                            child: Text("Mission: Draw a figure",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .subtitle1
+                                    ?.copyWith(color: Colors.grey[500]))),
+                        Container(
+                            margin: EdgeInsets.only(top: 10.0),
+                            child: Text(
+                                "Follow the Udemy course and draw a boot to practice contour and cross contour lines",
+                                style: Theme.of(context).textTheme.bodyText2))
+                      ])),
+            ])),
+        Container(
+          margin: EdgeInsets.only(top: 20.0),
+          child: Text("Logging",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  ?.copyWith(fontSize: 24)),
+        ),
+        Container(
+            margin: EdgeInsets.only(top: 10.0),
+            child: Row(children: <Widget>[
+              Expanded(
+                  child: Container(
+                      height: 50,
+                      margin: EdgeInsets.all(5.0),
+                      padding:
+                          EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
+                      ),
+                      child: Center(
+                          child: Text("🤔 Recall",
+                              style: Theme.of(context).textTheme.subtitle1)))),
+              Expanded(
+                  child: Container(
+                      height: 50,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                      margin: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
+                      ),
+                      child: Center(
+                          child: Text("🥊 Challenge",
+                              style: Theme.of(context).textTheme.subtitle1)))),
+            ])),
+        Row(children: <Widget>[
+          Expanded(
+              child: Container(
+                  height: 50,
+                  margin: EdgeInsets.all(5.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                  child: Center(
+                      child: Text("🔥 Progress",
+                          style: Theme.of(context).textTheme.subtitle1)))),
+          Expanded(
+              child: Container(
+                  height: 50,
+                  padding:
+                      EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                  margin: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                  child: Center(
+                      child: Text("🧠 Cards",
+                          style: Theme.of(context).textTheme.subtitle1)))),
+        ])
+      ]),
+    ))));
+  }
+}
+
 class ExplorePage extends StatefulWidget {
   ExplorePage({Key? key}) : super(key: key);
 
@@ -83,10 +250,7 @@ class _HomePageState extends State<HomePage> {
 
   List<Widget> _widgetOptions = <Widget>[
     ExplorePage(),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
+    LogPage(),
     Text(
       'Index 2: School',
       style: optionStyle,
@@ -107,7 +271,7 @@ class _HomePageState extends State<HomePage> {
           preferredSize: const Size.fromHeight(80.0),
           child: SafeArea(
               child: Container(
-                  margin: EdgeInsets.only(top: 10.0),
+                  margin: EdgeInsets.symmetric(vertical: 10.0),
                   padding: EdgeInsets.symmetric(horizontal: 30.0),
                   child: Row(children: <Widget>[
                     Container(
@@ -143,11 +307,11 @@ class _HomePageState extends State<HomePage> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.biotech),
-            label: 'Missions',
+            label: 'Mission Log',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.face),
-            label: 'Me',
+            label: 'Journey',
           ),
         ],
         currentIndex: _selectedIndex,
